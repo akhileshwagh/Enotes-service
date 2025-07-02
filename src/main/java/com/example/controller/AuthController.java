bfaa55c404f34fdfa0fb36036ef237a44883b42f
akhileshwagh <akhimech05@gmail.com>
2025-07-02 18:21:41 +0530

    verify email for account activation implemented

--- src/main/java/com/example/controller/AuthController.java
+++ src/main/java/com/example/controller/AuthController.java
@@ -13,14 +13,14 @@ import com.example.service.UserService;
 import com.example.util.CommonUtil;
 
 @RestController
-@RequestMapping("/api/v1/user")
+@RequestMapping("/api/v1/auth")
 public class AuthController {
 
 	@Autowired
 	private UserService userService;
 
 	@PostMapping("/")
-	public ResponseEntity<?> registerUser(@RequestBody UserDto userDto) {
+	public ResponseEntity<?> registerUser(@RequestBody UserDto userDto) throws Exception {
 		Boolean register = userService.register(userDto);
 		if (register) {
 			return CommonUtil.createBuildResponseMessage("Register success", HttpStatus.CREATED);
